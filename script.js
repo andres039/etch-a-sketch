96fc84fcae58c2a9b8516fb2f4dea581e0e8e394
Andres <andres039039@hotmail.com>
2021-07-14 17:23:22 -0400

    Main idea of the etch a sketch working

--- script.js
+++ script.js
@@ -1,17 +1,80 @@
-const container = document.getElementById('container');
+// Create a webpage with a 16x16 grid of square divs
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++){
-const square = document.createElement('div');
-square.classList.add('squares');
-container.appendChild(square);
-    }
+//     Create the divs using JavaScript… don’t try making them by hand with copy and pasting in your html file!
+const container = document.querySelector('#container');
+let resetButton = document.getElementById('reset');
+const squares = document.getElementsByClassName('div')
+window.onload = drawGrid(16)
+  function drawGrid(x) {
+  for (let i = 0; i < (x ** 2); i++) {
+    const divs = document.createElement('div');
+    container.setAttribute('style', `grid-template-rows: repeat(${x}, 1.3em);
+    grid-template-columns: repeat(${x}, 1.3em);`)
+    divs.setAttribute(
+      'style',
+      'background-color: wheat; border: 1px solid black'
+    );
+    divs.classList = 'div';
+    divs.id = 'div';
+    container.appendChild(divs);
+  }
+}
+function generateRandomColors () {
+  let str = []
+  for (let i = 0; i < 3; i++) {
+    str.push(Math.floor(Math.random() * 255));
+  }
+  return(`background-color: rgb(${str})`)
+}
+function reset () {
+  container.innerHTML = ''
+}
+function resize () {
+  let newSize = prompt('Please enter a numeric value for a new size')
+  newSize > 100 ? alert('please enter a anumber below 100'): drawGrid(newSize);
 }
 
 
-const randomColor = `hsl(${Math.floor(Math.random() * 360)}, ${Math.floor(Math.random() * 100)}%, ${Math.floor(Math.random() * 100)}%)`
+const allTheButtons = document.querySelector('nav')
+allTheButtons.addEventListener('click', (x) => {
+  if (x.target.id === 'reset') {
+    reset();
+    resize();
+  }
+  if (x.target.id === 'colorbutton') {
+    container.addEventListener('mouseover', (e) => {
+      if (e.target.id === 'div') {
+        e.target.setAttribute('style', generateRandomColors())
+      }
+    })
+  }
+  if (x.target.id === 'black') {
+    container.addEventListener('mouseover', (e) => {
+      if (e.target.id === 'div') {
+        e.target.setAttribute('style', 'background-color: black')
+      }
+    })
+  }
+  if (x.target.id === 'shady') {
+    container.addEventListener('mouseover', (e) => {
+      if (e.target.id === 'div') {
+        e.target.setAttribute('style', shadesOfBlack())
+      }
+    })
+    //inkColor depends on the current color that's been used. If we have a 10% then add 10%
+    //somehtingg like if (percent === 10%) {percent += 10}
+    //therefore, we need to figure out what percentage do we currently have
+    //hipotesis: try to change the gray from 10 to 100 just by going over it once
+   
+    function shadesOfBlack () {
+    let percent = 10
+    let rgbColor = `background-color: rgb(0, 0, 0, ${percent}%)`
 
-
-document.addEventListener(onmouseover,function(e) {
-    e.target.style.background =  randomColor;
+    if (e.target.style.backgroundColor === 'rgb(0, 0, 0, 10%') {
+      e.target.style.backgroundColor = 'rgb(0, 0, 0, 40%)'
+    }
+   
+    }
+   
+  }
 })
